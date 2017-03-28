c54428b69721c5eef71537c1ade49788b06ffae8
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-03-28 19:43:45 -0300

    Correct logout

--- resources/views/layouts/includes/navbar.blade.php
+++ resources/views/layouts/includes/navbar.blade.php
@@ -6,13 +6,18 @@
                 <li><a href="/">Home</a></li>
                 @unless (auth()->check())
                     <li><a href="/login">Login</a></li>
-                    <li><a href="/legister">Register</a></li>
+                    <li><a href="/register">Register</a></li>
                 @endunless
                 @if (auth()->check())
                     <li><a href="/recipe/create">Create Recipe</a></li>
                     <li><a class="dropdown-button" href="#!" data-activates="dropdown1">{{ auth()->user()->name }}<i class="material-icons right">arrow_drop_down</i></a></li>
                     <ul id="dropdown1" class="dropdown-content">
-                        <li><a href="/logout">Logout</a></li>
+                        <form action="/logout" method="POST">
+                            {{ csrf_field() }}
+                            <li>
+                                <button type="submit">Logout</button>
+                            </li>
+                        </form>
                     </ul>
                 @endif
             </ul>
