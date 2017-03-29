cd306e844a245ce2e07959f8c34181bb8fb3a212
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-03-28 21:22:55 -0300

    Add cover picture to show page

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <img src="{{ $recipe->cover }}" alt="{{ $recipe->title }}">
     <h1>{{ $recipe->title }}</h1>
     @if ($recipe->isOwner())
         <a href="/recipe/{{ $recipe->id }}/update">Edit</a>
