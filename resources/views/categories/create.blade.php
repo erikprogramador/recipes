5e9b8fc403683b93a2c4430f11e8c22e87183e44
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-03-31 23:42:27 -0300

    Add Styling to create category page

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Create a category</h1>
-    <form action="/category/store" method="POST">
-        {{ csrf_field() }}
-        <label for="title">Title</label>
-        <input type="text" name="title" id="title">
-        <button type="submit">Create</button>
-    </form>
+    <div class="container">
+        <div class="row">
+            <div class="col m6 offset-m3 s12">
+                <h2>Create a category</h2>
+                <form action="/category/store" method="POST">
+                    {{ csrf_field() }}
+                    <label for="title">Title</label>
+                    <input type="text" name="title" id="title">
+                    <div class="center">
+                        <button class="btn-floating btn-large waves-effect waves-light red" type="submit">
+                            <i class="material-icons">add</i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
