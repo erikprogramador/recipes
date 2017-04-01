f4a2d43452f5c7ea7c0a7167d1ae5ddb91158e9f
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-03-31 23:37:21 -0300

    Stilyng email and email reset views

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,19 +4,24 @@
     @if (session('status'))
         {{ session('status') }}
     @endif
+    <div class="container auth-form">
+        <div class="row">
+            <div class="col m6 offset-m3 s12">
+                <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
+                    {{ csrf_field() }}
 
-    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-        {{ csrf_field() }}
+                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
-        <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                    @if ($errors->has('email'))
+                        <strong>{{ $errors->first('email') }}</strong>
+                    @endif
 
-        @if ($errors->has('email'))
-            <strong>{{ $errors->first('email') }}</strong>
-        @endif
-
-        <button type="submit" class="btn btn-primary">
-            Send Password Reset Link
-        </button>
-    </form>
+                    <button type="submit" class="btn btn-primary">
+                        Send Password Reset Link
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
