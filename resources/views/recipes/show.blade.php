d12a78785b534a507f4d94e2ae0382f1fba75232
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-03-31 23:54:08 -0300

    Styling show page

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,13 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <img src="{{ $recipe->cover }}" alt="{{ $recipe->title }}">
-    <h1>{{ $recipe->title }}</h1>
-    @if ($recipe->isOwner())
-        <a href="/recipe/{{ $recipe->id }}/update">Edit</a>
-        <form action="/recipe/{{ $recipe->id }}/delete" method="POST">
-            <button type="submit">Delete</button>
-        </form>
-    @endif
-    <p>{{ $recipe->description }}</p>
+    <div class="container">
+        <div class="row">
+            <div class="col s8 offset-s2">
+                <h1>{{ $recipe->title }}</h1>
+                <img class="responseive-img" width="100%" src="{{ $recipe->cover }}" alt="{{ $recipe->title }}">
+                @if ($recipe->isOwner())
+                    <div class="row">
+                        <div class="col s6 offset-s6">
+                            <div class="row">
+                                <div class="col col-6">
+                                    <a class="btn blue" href="/recipe/{{ $recipe->id }}/update">Edit</a>
+                                </div>
+                                <div class="col col-6">
+                                    <form action="/recipe/{{ $recipe->id }}/delete" method="POST">
+                                        <button class="btn red" type="submit">Delete</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+                <p>{{ $recipe->description }}</p>
+            </div>
+        </div>
+    </div>
 @endsection
