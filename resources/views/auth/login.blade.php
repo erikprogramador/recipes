c5380dc975c83e5621b0952482ed8ebacbd322fd
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-03-31 23:34:11 -0300

    Styling login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,33 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-        {{ csrf_field() }}
+    <div class="container auth-form">
+        <div class="row">
+            <div class="col m6 offset-m3 s12">
+                <form role="form" method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
 
-        <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
-        @if ($errors->has('email'))
-                <strong>{{ $errors->first('email') }}</strong>
-        @endif
+                    @if ($errors->has('email'))
+                            <strong>{{ $errors->first('email') }}</strong>
+                    @endif
 
-        <label for="password" class="col-md-4 control-label">Password</label>
-        <input id="password" type="password" class="form-control" name="password" required>
+                    <label for="password" class="col-md-4 control-label">Password</label>
+                    <input id="password" type="password" class="form-control" name="password" required>
 
-        @if ($errors->has('password'))
-            <strong>{{ $errors->first('password') }}</strong>
-        @endif
+                    @if ($errors->has('password'))
+                        <strong>{{ $errors->first('password') }}</strong>
+                    @endif
 
-        <label>
-            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-        </label>
+                    <label>
+                    </label>
 
-        <button type="submit" class="btn btn-primary">
-            Login
-        </button>
+                    <div>
+                        <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label for="remember">Remember Me</label>
+                    </div>
 
-        <a class="btn btn-link" href="{{ route('password.request') }}">
-            Forgot Your Password?
-        </a>
-    </form>
+                    <div class="input-field">
+                        <button type="submit" class="btn btn-primary">
+                            Login
+                        </button>
+                        <a href="{{ route('password.request') }}">
+                            Forgot Your Password?
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,33 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
-        {{ csrf_field() }}
+    <div class="container auth-form">
+        <div class="row">
+            <div class="col m6 offset-m3 s12">
+                <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+                    {{ csrf_field() }}
 
-        <label for="name" class="col-md-4 control-label">Name</label>
-        <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                    <label for="name" class="col-md-4 control-label">Name</label>
+                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
-        @if ($errors->has('name'))
-            <strong>{{ $errors->first('name') }}</strong>
-        @endif
+                    @if ($errors->has('name'))
+                        <strong>{{ $errors->first('name') }}</strong>
+                    @endif
 
-        <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
-        @if ($errors->has('email'))
-            <strong>{{ $errors->first('email') }}</strong>
-        @endif
+                    @if ($errors->has('email'))
+                        <strong>{{ $errors->first('email') }}</strong>
+                    @endif
 
-        <label for="password" class="col-md-4 control-label">Password</label>
-        <input id="password" type="password" class="form-control" name="password" required>
+                    <label for="password" class="col-md-4 control-label">Password</label>
+                    <input id="password" type="password" class="form-control" name="password" required>
 
-        @if ($errors->has('password'))
-            <strong>{{ $errors->first('password') }}</strong>
-        @endif
+                    @if ($errors->has('password'))
+                        <strong>{{ $errors->first('password') }}</strong>
+                    @endif
 
-        <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                    <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 
-        <button type="submit" class="btn btn-primary">Register</button>
-    </form>
+                    <button type="submit" class="btn btn-primary">Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
