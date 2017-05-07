a0e52ddcbc430fc31abb285d045a710312913464
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-05-06 23:04:40 -0300

    Remove collection call on test functions files

--- tests/utilities/functions.php
+++ tests/utilities/functions.php
@@ -1,9 +1,17 @@
 <?php
 
-function make($class, $overrides = [], $quantity = 1) {
-    return factory($class, $quantity)->make($overrides);
+function make($class, $attributes = []) {
+    return factory($class)->make($attributes);
 }
 
-function create($class, $overrides = [], $quantity = 1) {
-    return factory($class, $quantity)->create($overrides);
+function makeMany($class, $quantity = 2,  $attributes = []) {
+    return factory($class, $quantity)->make($attributes);
+}
+
+function create($class, $attributes = []) {
+    return factory($class)->create($attributes);
+}
+
+function createMany($class, $quantity = 2,  $attributes = []) {
+    return factory($class, $quantity)->create($attributes);
 }
