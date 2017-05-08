c482bdba827eed2027a2549a22b42f13454a5d83
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-05-07 23:37:31 -0300

    Correct make error

--- tests/Feature/FilterRecipesByCategoryTest.php
+++ tests/Feature/FilterRecipesByCategoryTest.php
@@ -13,11 +13,11 @@ class FilterRecipesByCategoryTest extends TestCase
     /** @test */
     function only_recipes_associate_with_a_given_category_must_shows_when_category_is_search()
     {
-        $categories = make(Category::class);
+        $categories = create(Category::class);
         $recipe = create(Recipe::class);
         $recipe->categories()->attach($categories);
         $noShow = create(Recipe::class, ['title' => 'ERIK XPTO']);
-        $noCategory = make(Category::class, ['title' => 'Chocolate cacke', 'slug' => 'chocolate-cacke']);
+        $noCategory = create(Category::class, ['title' => 'Chocolate cacke', 'slug' => 'chocolate-cacke']);
         $noShow->categories()->attach($noCategory);
 
         $this->get('/recipe/category/'. $categories->slug)
